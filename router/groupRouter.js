519c117442a728c90234aba724b680c562fed80e
emptybagelman <samuelmcgin@aol.com>
2023-12-08 01:26:22 +0000

    README.md

--- router/groupRouter.js
+++ router/groupRouter.js
@@ -7,5 +7,7 @@ router.get("/", groupController.index)
 router.get("/id/:id",groupController.showId)
 router.get("/name/:name",groupController.showName)
 router.get("/creatorId/:id", groupController.showCreator)
+router.post("/",groupController.createGroup)
+router.post("/join",groupController.joinGroup)
 
 module.exports = router;
