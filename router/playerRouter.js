010c527db0f50ad0507e1894e36411f1c4914dce
emptybagelman <samuelmcgin@aol.com>
2023-12-10 21:24:09 +0000

    fixed GET all players by quiz_id route

--- router/playerRouter.js
+++ router/playerRouter.js
@@ -5,7 +5,7 @@ const playerController = require("../controller/playerController")
 
 router.get("/",playerController.index)
 router.get("/:id",playerController.showId)
-router.get("/quiz/:id",playerController.showQuizId)
+router.get("/quiz/:quiz_id",playerController.showQuizId)
 router.post("/",playerController.create)
 router.delete("/:id",playerController.destroy)
 
